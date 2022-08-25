0362073cdee01e2c0927f34e1f69c3d9fe63b76d
Franco Bernardi <francobernardi134@gmail.com>
2022-08-25 19:12:30 -0300

    Se agrego la capacidad de buscar por dominio

--- JS/script.js
+++ JS/script.js
@@ -21,8 +21,8 @@ const getClientIp = () => {
     .then(data => getIpLocation(data.ip));
 }
 
-const getIpLocation = (ipAddress) => {
-    fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_lgks77mikFqR3RFCG0fYrAFDjisk4&ipAddress=${ipAddress}`)  
+const getIpLocation = (search) => {
+    fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_lgks77mikFqR3RFCG0fYrAFDjisk4&ipAddress=${search}&domain=${search}`)  
     .then(response => response.json())
     .then(data => {
         generateMap(data.location.lat,data.location.lng);
